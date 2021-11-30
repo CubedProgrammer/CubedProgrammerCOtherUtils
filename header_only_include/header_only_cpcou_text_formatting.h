e6e2d7a21aa3d21c03b4cbc41370771944cb2ce1
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2021-11-30 12:43:58 -0500

    Added colour formatting.

--- header_only_include/header_only_cpcou_text_formatting.h
+++ header_only_include/header_only_cpcou_text_formatting.h
@@ -12,6 +12,52 @@ cpcou_text_formatter_type cpcou_fmt_crossed = "\033\1339m";
 cpcou_text_formatter_type cpcou_fmt_overlined = "\033\13353m";
 cpcou_text_formatter_type cpcou_fmt_def_background = "\033\13349m";
 cpcou_text_formatter_type cpcou_fmt_def_foreground = "\033\13339m";
+cpcou_text_formatter_type cpcou_fmt_black_background = "\033\13340m";
+cpcou_text_formatter_type cpcou_fmt_red_background = "\033\13341m";
+cpcou_text_formatter_type cpcou_fmt_green_background = "\033\13342m";
+cpcou_text_formatter_type cpcou_fmt_yellow_background = "\033\13343m";
+cpcou_text_formatter_type cpcou_fmt_blue_background = "\033\13344m";
+cpcou_text_formatter_type cpcou_fmt_magenta_background = "\033\13345m";
+cpcou_text_formatter_type cpcou_fmt_cyan_background = "\033\13346m";
+cpcou_text_formatter_type cpcou_fmt_white_background = "\033\13347m";
+cpcou_text_formatter_type cpcou_fmt_black_foreground = "\033\13330m";
+cpcou_text_formatter_type cpcou_fmt_red_foreground = "\033\13331m";
+cpcou_text_formatter_type cpcou_fmt_green_foreground = "\033\13332m";
+cpcou_text_formatter_type cpcou_fmt_yellow_foreground = "\033\13333m";
+cpcou_text_formatter_type cpcou_fmt_blue_foreground = "\033\13334m";
+cpcou_text_formatter_type cpcou_fmt_magenta_foreground = "\033\13335m";
+cpcou_text_formatter_type cpcou_fmt_cyan_foreground = "\033\13336m";
+cpcou_text_formatter_type cpcou_fmt_white_foreground = "\033\13337m";
+
+char cpcou____fmt_any_colour_str[20];
+
+/**
+ * Get formatter for any colour.
+ * A call to one of these functions will modify the return values of previous calls
+ * Therefore, it is wise to use the return value of these functions immediately
+ * If a colour is used often, its call to one of these functions should be defined as a macro
+ */
+cpcou_text_formatter_type cpcou_fmt_background_cmy(int c, int m, int y)
+{
+	return cpcou_fmt_background_rgb(255 - c, 255 - m, 255 - y);
+}
+
+cpcou_text_formatter_type cpcou_fmt_background_rgb(int r, int g, int b)
+{
+	sprintf(cpcou____fmt_any_colour_str, "\033\13348;2;%i;%i;%im", r, g, b);
+	return cpcou____fmt_any_colour_str;
+}
+
+cpcou_text_formatter_type cpcou_fmt_foreground_cmy(int c, int m, int y)
+{
+	return cpcou_fmt_foreground_rgb(255 - c, 255 - m, 255 - y);
+}
+
+cpcou_text_formatter_type cpcou_fmt_foreground_rgb(int r, int g, int b)
+{
+	sprintf(cpcou____fmt_any_colour_str, "\033\13338;2;%i;%i;%im", r, g, b);
+	return cpcou____fmt_any_colour_str;
+}
 
 /**
  * Sets the text formatting of a file
--- include/cpcou_text_formatting.h
+++ include/cpcou_text_formatting.h
@@ -15,6 +15,33 @@ extern cpcou_text_formatter_type cpcou_fmt_crossed;
 extern cpcou_text_formatter_type cpcou_fmt_overlined;
 extern cpcou_text_formatter_type cpcou_fmt_def_background;
 extern cpcou_text_formatter_type cpcou_fmt_def_foreground;
+extern cpcou_text_formatter_type cpcou_fmt_black_background;
+extern cpcou_text_formatter_type cpcou_fmt_red_background;
+extern cpcou_text_formatter_type cpcou_fmt_green_background;
+extern cpcou_text_formatter_type cpcou_fmt_yellow_background;
+extern cpcou_text_formatter_type cpcou_fmt_blue_background;
+extern cpcou_text_formatter_type cpcou_fmt_magenta_background;
+extern cpcou_text_formatter_type cpcou_fmt_cyan_background;
+extern cpcou_text_formatter_type cpcou_fmt_white_background;
+extern cpcou_text_formatter_type cpcou_fmt_black_foreground;
+extern cpcou_text_formatter_type cpcou_fmt_red_foreground;
+extern cpcou_text_formatter_type cpcou_fmt_green_foreground;
+extern cpcou_text_formatter_type cpcou_fmt_yellow_foreground;
+extern cpcou_text_formatter_type cpcou_fmt_blue_foreground;
+extern cpcou_text_formatter_type cpcou_fmt_magenta_foreground;
+extern cpcou_text_formatter_type cpcou_fmt_cyan_foreground;
+extern cpcou_text_formatter_type cpcou_fmt_white_foreground;
+
+/**
+ * Get formatter for any colour.
+ * A call to one of these functions will modify the return values of previous calls
+ * Therefore, it is wise to use the return value of these functions immediately
+ * If a colour is used often, its call to one of these functions should be defined as a macro
+ */
+cpcou_text_formatter_type cpcou_fmt_background_cmy(int c, int m, int y);
+cpcou_text_formatter_type cpcou_fmt_background_rgb(int r, int g, int b);
+cpcou_text_formatter_type cpcou_fmt_foreground_cmy(int c, int m, int y);
+cpcou_text_formatter_type cpcou_fmt_foreground_rgb(int r, int g, int b);
 
 /**
  * Sets the text formatting of a file
